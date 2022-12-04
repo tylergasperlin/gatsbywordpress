ff8f78befee12ee353d83600f22d4e5bf803c2bc
tylergasperlin <gasperlint@gmail.com>
2022-12-04 13:33:17 -0800

    Query specific menu

--- src/components/mainMenu.js
+++ src/components/mainMenu.js
@@ -5,9 +5,15 @@ const MainMenu = () => {
   return (
     <StaticQuery
       query={graphql`
-        {
-          allWordpressWpApiMenusMenusItems {
-            nodes {
+      {
+        allWordpressWpApiMenusMenusItems(filter: {
+          name: {
+            eq: "main"
+          }
+        }) {
+          edges {
+            node {
+              name
               items {
                 title
                 object_slug
@@ -15,10 +21,13 @@ const MainMenu = () => {
             }
           }
         }
+      }
+      
       `}
       render={props => (
         <div>
-          {props.allWordpressWpApiMenusMenusItems.nodes[0].items.map(
+            {/* {console.log(props.allWordpressWpApiMenusMenusItems.edges)} */}
+          {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(
             item => (
               <Link to={`/${item.object_slug}`} key={item.title}>{item.title}</Link>
             )
